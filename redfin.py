47b406e243aeb8e9fcb3e9019d840d7163eeef8a
Asad Ijaz <asad.ijaz585@gmail.com>
2023-02-24 15:21:53 +0500

    pep8 fixes

--- redfin.py
+++ redfin.py
@@ -11,19 +11,22 @@ urls = []
 
 result = requests.get('https://www.redfin.com/city/2471/CA/Calabasas/filter/include=sold-5yr', headers=header)
 pages_count = BeautifulSoup(result.content, 'html.parser')
+
 for page in pages_count.find_all('a', {'class': 'clickable goToPage'})[::-7]:
     # manual verification for pages
     print(page.text.strip(),'pages')
+
     for i in range(1,int(page.text.strip())):
         results = requests.get('https://www.redfin.com/city/2471/CA/Calabasas/filter/include=sold-5yr/page-{}'.format(i), headers=header)
         data = BeautifulSoup(results.content, 'html.parser')
         for tag in data.find_all('div', {'class': 'scrollable'}):
             url = 'https://www.redfin.com'+tag.find('a').get('href')
             urls.append(url)
-    Column_Header = ['price', 'beds', 'baths', 'URL', 'Sqft', 'Adress', 'state', 'zip_code', 'Sold Price', 'Sold Date']
+    column_header = ['Price', 'Beds', 'Baths', 'URL', 'Sqft', 'Address', 'State', 'Zip_code', 'Sold Price', 'Sold Date']
+
     with open('redfin.csv', 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(Column_Header)
+        writer.writerow(column_header)
 
         for url in urls:
             result_url = requests.get(url, headers=header)
@@ -33,22 +36,22 @@ for page in pages_count.find_all('a', {'class': 'clickable goToPage'})[::-7]:
             price = overview[0].text
             beds = overview[1].text
             baths = overview[2].text
-            URL = url
+            url_object = url
             # Sqft
             square_foot = soup_data_url.find_all('div', {'class': 'stat-block sqft-section'})
-            Square_ft = square_foot[0].text
-            adress = soup_data_url.find_all('div', {'class': 'homeAddress'})
-            Adress = adress[0].text
-            state = adress[0].text.split(' ')[-2]
-            zip_code = adress[0].text.split(' ')[-1]
+            square_ft = square_foot[0].text
+            address_object = soup_data_url.find_all('div', {'class': 'homeAddress'})
+            address = address_object[0].text
+            state = address[0].text.split(' ')[-2]
+            zip_code = address[0].text.split(' ')[-1]
             sold_class = soup_data_url.find_all('div', {'class': 'secondary-info'})
             if sold_class[0].text.split(' ')[5] != 'ago.':
-                SOLD = sold_class[0].text.split(' ')[5]
+                sold = sold_class[0].text.split(' ')[5]
             else:
-                SOLD = ' '
-            date = soup_data_url.find_all('div', {'class': 'secondary-info'})
-            DATE = date[0].text.split(' ')[-3]+' '+date[0].text.split(' ')[-2]+date[0].text.split(' ')[-1]
-            data_to_write = [price, beds, baths, URL, Square_ft, Adress, state, zip_code, SOLD, DATE]
+                sold = ' '
+            date_object = soup_data_url.find_all('div', {'class': 'secondary-info'})
+            date = date_object[0].text.split(' ')[-3]+' '+date_object[0].text.split(' ')[-2]+date_object[0].text.split(' ')[-1]
+            data_to_write = [price, beds, baths, url_object, square_ft, address, state, zip_code, sold, date]
             writer.writerow(data_to_write)
         break
     break
